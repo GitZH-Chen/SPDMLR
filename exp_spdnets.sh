c0ac1a88ce94e97afffe3064674fa4c0a217e006
ZH Chen <ziheng_ch@163.com>
2024-03-19 23:55:27 +0100

    111

--- exp_spdnets.sh
+++ exp_spdnets.sh
@@ -17,9 +17,9 @@ hdm05_path=/data #change this to your data folder
 ### Experiments on SPDNet
 [ $? -eq 0 ] && python SPDNet-MLR -m dataset=HDM05 dataset.path=hdm05_path nnet.model.architecture=[93,30],[93,70,30],[93,70,50,30] nnet.model.classifier=LogEigMLR
 ### Experiments on SPDNet-LEM
-[ $? -eq 0 ] && python SPDNet-MLR -m dataset=HDM05 dataset.path=hdm05_path nnet.model.architecture=[93,30],[93,70,30],[93,70,50,30] nnet.model.classifier=LogEigMLR\
+[ $? -eq 0 ] && python SPDNet-MLR -m dataset=HDM05 dataset.path=hdm05_path nnet.model.architecture=[93,30],[93,70,30],[93,70,50,30] nnet.model.classifier=SPDMLR\
 nnet.model.metric=SPDLogEuclideanMetric
 
 ### Experiments on SPDNet-LCM
-[ $? -eq 0 ] && python SPDNet-MLR -m dataset=HDM05 dataset.path=hdm05_path nnet.model.architecture=[93,30],[93,70,30],[93,70,50,30] nnet.model.classifier=LogEigMLR\
-nnet.model.metric=SPDLogCholeskyMetric
+[ $? -eq 0 ] && python SPDNet-MLR -m dataset=HDM05 dataset.path=hdm05_path nnet.model.architecture=[93,30],[93,70,30],[93,70,50,30] nnet.model.classifier=SPDMLR\
+nnet.model.metric=SPDLogCholeskyMetric nnet.model.power=1.,0.5
