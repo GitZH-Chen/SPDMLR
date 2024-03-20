1516c83b0e7e2e587c2d6ceb4d34b3e3edf8f786
ZH Chen <ziheng_ch@163.com>
2024-03-20 11:41:02 +0100

    111

--- exp_spdnets.sh
+++ exp_spdnets.sh
@@ -3,7 +3,7 @@
 #----RADAR----
 radar_path=/data #change this to your data folder
 ### Experiments on SPDNet
-[ $? -eq 0 ] && python SPDNet-MLR -m dataset=RADAR dataset.path=radar_path nnet.model.architecture=[20,16,14,12,10,8],[20,16,8] nnet.model.classifier=LogEigMLR
+[ $? -eq 0 ] && python SPDNet-MLR.py -m dataset=RADAR dataset.path=radar_path nnet.model.architecture=[20,16,14,12,10,8],[20,16,8] nnet.model.classifier=LogEigMLR
 ### Experiments on SPDNet-LEM
 [ $? -eq 0 ] && python SPDNet-MLR.py -m dataset=RADAR dataset.path=radar_path nnet.model.architecture=[20,16,14,12,10,8],[20,16,8] nnet.model.classifier=SPDMLR\
  nnet.model.metric=SPDLogEuclideanMetric nnet.model.beta=1.,0.
@@ -15,11 +15,11 @@ radar_path=/data #change this to your data folder
 #----HDM05----
 hdm05_path=/data #change this to your data folder
 ### Experiments on SPDNet
-[ $? -eq 0 ] && python SPDNet-MLR -m dataset=HDM05 dataset.path=hdm05_path nnet.model.architecture=[93,30],[93,70,30],[93,70,50,30] nnet.model.classifier=LogEigMLR
+[ $? -eq 0 ] && python SPDNet-MLR.py -m dataset=HDM05 dataset.path=hdm05_path nnet.model.architecture=[93,30],[93,70,30],[93,70,50,30] nnet.model.classifier=LogEigMLR
 ### Experiments on SPDNet-LEM
-[ $? -eq 0 ] && python SPDNet-MLR -m dataset=HDM05 dataset.path=hdm05_path nnet.model.architecture=[93,30],[93,70,30],[93,70,50,30] nnet.model.classifier=SPDMLR\
+[ $? -eq 0 ] && python SPDNet-MLR.py -m dataset=HDM05 dataset.path=hdm05_path nnet.model.architecture=[93,30],[93,70,30],[93,70,50,30] nnet.model.classifier=SPDMLR\
 nnet.model.metric=SPDLogEuclideanMetric
 
 ### Experiments on SPDNet-LCM
-[ $? -eq 0 ] && python SPDNet-MLR -m dataset=HDM05 dataset.path=hdm05_path nnet.model.architecture=[93,30],[93,70,30],[93,70,50,30] nnet.model.classifier=SPDMLR\
+[ $? -eq 0 ] && python SPDNet-MLR.py -m dataset=HDM05 dataset.path=hdm05_path nnet.model.architecture=[93,30],[93,70,30],[93,70,50,30] nnet.model.classifier=SPDMLR\
 nnet.model.metric=SPDLogCholeskyMetric nnet.model.power=1.,0.5
